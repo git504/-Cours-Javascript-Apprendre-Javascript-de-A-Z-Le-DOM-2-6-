e24a6d84c21ca812b45a2e44688f1240f8795348
git504 <git504@mailo.com>
2021-05-30 16:51:14 +0200

    DOMAZ

---
+++ JS-A-to-Z/cours/index.js
@@ -0,0 +1,15 @@
+//SELECTEUR
+
+//click event
+const questionContainer = document.querySelector(".click-event");
+console.log(questionContainer);
+
+questionContainer.style.borderRadius = "150px";
+questionContainer.addEventListener("click", () => {
+    console.log(questionContainer);
+    // questionContainer.style.background = "red";
+    // questionContainer.style.border = "3px solid teal";
+})
+
+
+
