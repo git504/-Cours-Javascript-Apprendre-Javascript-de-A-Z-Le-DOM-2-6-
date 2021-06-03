3ed8dd7e89e5409baf9d28ce05c4a14546142cd4
git504 <git504@mailo.com>
2021-06-03 10:36:36 +0200

    JAZ - 2

--- JS-A-to-Z/cours/index.js
+++ JS-A-to-Z/cours/index.js
@@ -107,21 +107,26 @@ const form = document.querySelector("form");
 let pseudo = "";
 let language = "";
 
-inputName.addEventListener('input', (e) => {
+inputName.addEventListener("input", (e) => {
   pseudo = e.target.value;
 });
 
-select.addEventListener('input', (e) => {
+select.addEventListener("input", (e) => {
   language = e.target.value;
 });
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault;
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
   if (cgv.checked) {
-
+    document.querySelector("form > div").innerHTML = `
+      <h3>Pseudo : ${pseudo}</h3>
+      <h4>Langage préféré : ${language}</h4>
+    `;
   } else {
-    alert("Veuillez accepter les conditions gnérales de vente")
+    alert("Veuillez accepter les CGV");
   }
 });
 
+//------------------------------------------------
+// Load event
