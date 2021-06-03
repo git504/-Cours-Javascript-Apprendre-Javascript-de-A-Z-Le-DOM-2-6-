7064b48970dd218c5f60394736cd5eba85c0fea4
git504 <git504@mailo.com>
2021-06-03 07:42:19 +0200

    JAZ - 2

--- JS-A-to-Z/cours/index.js
+++ JS-A-to-Z/cours/index.js
@@ -2,16 +2,29 @@
 
 //click event
 const questionContainer = document.querySelector(".click-event");
-console.log(questionContainer);
+//console.log(questionContainer);
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+//console.log(btn1, btn2);
+const response = document.querySelector("p");
+//console.log(response);
 
 questionContainer.style.borderRadius = "150px";
+
 questionContainer.addEventListener("click", () => {
-    console.log(questionContainer);
-    // questionContainer.style.background = "red";
-    // questionContainer.style.border = "3px solid teal";
-questionContainer.classList.toggle("question-clicked")
+  //console.log(questionContainer);
+  // questionContainer.style.background = "red";
+  // questionContainer.style.border = "3px solid teal";
+  questionContainer.classList.toggle("question-clicked");
+});
 
-})
+btn1.addEventListener("click", () => {
+  response.classList.add("show-response");
+  response.style.background = "green";
+});
 
-
+btn2.addEventListener("click", () => {
+  response.classList.add("show-response");
+  response.style.background = "red";
+});
 
